9636a926dadb9906b70fdfb28398bc719bef683d
Yoshiteru Nagata <yositeru1108@nagoya-u.jp>
2022-08-22 01:33:59 +0900

    creating

--- pc_info_db.go
+++ pc_info_db.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
 	"flag"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
 	"os"
@@ -25,7 +23,7 @@ import (
 
 	//	"github.com/mtfelian/golang-socketio/transport"
 	//	pcounter "github.com/synerex/proto_pcounter"
-	protoPF "github.com/UCLabNU/proto_pflow"
+
 	protoPC "github.com/nagata-yoshiteru/proto_pcounter"
 	api "github.com/synerex/synerex_api"
 	pbase "github.com/synerex/synerex_proto"
@@ -47,24 +45,12 @@ var (
 	sxServerAddress string
 	pcMu            *sync.Mutex = nil
 	pcLoop          *bool       = nil
-	pfMu            *sync.Mutex = nil
-	pfLoop          *bool       = nil
 	db              *sql.DB
 	db_host         = os.Getenv("MYSQL_HOST")
 	db_name         = os.Getenv("MYSQL_DATABASE")
 	db_user         = os.Getenv("MYSQL_USER")
 	db_pswd         = os.Getenv("MYSQL_PASSWORD")
 )
-
-type PFJ struct {
-	ID         uint32
-	FromSID    uint32
-	FromTime   int64
-	FromHeight uint32
-	ToSID      uint32
-	ToTime     int64
-	ToHeight   uint32
-}
 
 type PCJ struct {
 	SID  uint8
@@ -86,7 +72,7 @@ func init() {
 	if err != nil {
 		print("connection error: ")
 		print(err)
-		print("\n")
+		log.Fatal("\n")
 	}
 
 	// ping
@@ -94,275 +80,35 @@ func init() {
 	if err != nil {
 		print("ping error: ")
 		print(err)
-		print("\n")
+		log.Fatal("\n")
 	}
 
-	// create table
-	_, err = db.Exec(`create table if not exists pf(id BIGINT unsigned not null auto_increment, src_time DATETIME(3) not null, src_sid INT unsigned not null, dst_time DATETIME(3) not null, dst_sid INT unsigned not null, primary key(id))`)
-	// select hex(mac) from log;
-	// insert into pf (mac) values (x'000CF15698AD');
+	// create users table
+	_, err = db.Exec(`create table if not exists users(id BIGINT unsigned NOT NULL AUTO_INCREMENT, sub CHAR(36) NOT NULL, username VARCHAR(64) NOT NULL, email VARCHAR(256) NOT NULL, primary key(id))`)
 	if err != nil {
-		print("exec error: ")
+		print("create users table error: ")
 		print(err)
-		print("\n")
+		log.Fatal("\n")
+	}
+
+	// create data_sources table
+	_, err = db.Exec(`create table if not exists data_sources(id BIGINT unsigned NOT NULL AUTO_INCREMENT, user_id BIGINT unsigned NOT NULL, type INT NOT NULL, latitude DOUBLE, longitude DOUBLE, radius DOUBLE, opt VARCHAR(1024), foreign key fk_user_id (user_id) references users(id), primary key(id))`)
+	if err != nil {
+		print("create data_sources table error: ")
+		print(err)
+		log.Fatal("\n")
+	}
+
+	// create perms table
+	_, err = db.Exec(`create table if not exists perms(id BIGINT unsigned NOT NULL AUTO_INCREMENT, user_id BIGINT unsigned NOT NULL, data_source_id BIGINT unsigned NOT NULL, granularity_time INT, granularity_mesh INT, opt VARCHAR(1024), foreign key fk_user_id (user_id) references users(id), foreign key fk_data_source_id (data_source_id) references data_sources(id), primary key(id))`)
+	if err != nil {
+		print("create perms table error: ")
+		print(err)
+		log.Fatal("\n")
 	}
 }
 
-func parseDate(urlArr []string, start_idx int) (string, string) {
-	sYear, err := strconv.Atoi(urlArr[start_idx])
-	if err != nil {
-		return "err", "wrong start year"
-	}
-	sMonth, _ := strconv.Atoi(urlArr[start_idx+1])
-	if err != nil {
-		return "err", "wrong start month"
-	}
-	sDay, _ := strconv.Atoi(urlArr[start_idx+2])
-	if err != nil {
-		return "err", "wrong start day"
-	}
-	eYear, err := strconv.Atoi(urlArr[start_idx+3])
-	if err != nil {
-		return "err", "wrong end year"
-	}
-	eMonth, _ := strconv.Atoi(urlArr[start_idx+4])
-	if err != nil {
-		return "err", "wrong end month"
-	}
-	eDay, _ := strconv.Atoi(urlArr[start_idx+5])
-	if err != nil {
-		return "err", "wrong end day"
-	}
-	stime := fmt.Sprintf("%04d-%02d-%02d 00:00:00.000", sYear, sMonth, sDay)
-	etime := fmt.Sprintf("%04d-%02d-%02d 23:59:59.999", eYear, eMonth, eDay)
-	return stime, etime
-}
-
-// apiHandler for PCounter/PFlow/WT Data
-func apiHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet && r.Method != http.MethodHead {
-		return
-	}
-
-	urlPath := r.URL.Path
-	urlArr := strings.Split(urlPath, "/")
-
-	switch urlArr[2] {
-	case "pc":
-		if len(urlArr) < 10 {
-			io.WriteString(w, "PC: too few arguments")
-			return
-		}
-		sid, err := strconv.Atoi(urlArr[3])
-		if err != nil {
-			io.WriteString(w, "PC: wrong src sid")
-			return
-		}
-		stime, etime := parseDate(urlArr, 4)
-		if stime == "err" {
-			io.WriteString(w, fmt.Sprintf("PC: %s", etime))
-		}
-		fmt.Println(time.Now())
-		rows, err := db.Query(`select id, time, dir, height from pc where sid = ? and time BETWEEN ? and ?`, sid, stime, etime)
-		if err != nil {
-			print(err)
-			io.WriteString(w, "PC: query error")
-		}
-		fmt.Println(time.Now())
-		var str_build strings.Builder
-		print(rows)
-		for rows.Next() {
-			var id int64
-			var time, dir string
-			var height int
-			err := rows.Scan(&id, &time, &dir, &height)
-			if err != nil {
-				print(err)
-				io.WriteString(w, "PC: scan error")
-			}
-			str_build.WriteString(fmt.Sprintf("%d,%s,%s,%d\n", id, time, dir, height))
-			//print(fmt.Sprintf("%d: %s %s %d\n", id, time, dir, height))
-		}
-		fmt.Println(time.Now())
-		output := str_build.String()
-		io.WriteString(w, output)
-		fmt.Println(time.Now())
-		return
-	case "pca":
-		if len(urlArr) < 9 {
-			io.WriteString(w, "PCA: too few arguments")
-			return
-		}
-		stime, etime := parseDate(urlArr, 3)
-		if stime == "err" {
-			io.WriteString(w, fmt.Sprintf("PCA: %s", etime))
-		}
-		fmt.Println(time.Now())
-		rows, err := db.Query(`select id, sid, time, dir, height from pc where time BETWEEN ? and ?`, stime, etime)
-		if err != nil {
-			print(err)
-			io.WriteString(w, "PCA: query error")
-		}
-		fmt.Println(time.Now())
-		var str_build strings.Builder
-		for rows.Next() {
-			var id, sid int64
-			var time, dir string
-			var height int
-			err := rows.Scan(&id, &sid, &time, &dir, &height)
-			if err != nil {
-				print(err)
-				io.WriteString(w, "PCA: scan error")
-			}
-			str_build.WriteString(fmt.Sprintf("%d,%d,%s,%s,%d\n", id, sid, time, dir, height))
-			// print(fmt.Sprintf("%d[%d]: %s %s %d\n", id, sid, time, dir, height))
-		}
-		fmt.Println(time.Now())
-		output := str_build.String()
-		io.WriteString(w, output)
-		fmt.Println(time.Now())
-		return
-	case "pf":
-		io.WriteString(w, "PF API")
-		return
-	case "pfwt":
-		if len(urlArr) < 10 {
-			io.WriteString(w, "WT: too few arguments")
-			return
-		}
-		src, err := strconv.Atoi(urlArr[3])
-		if err != nil {
-			io.WriteString(w, "WT: wrong src id")
-			return
-		}
-		stime, etime := parseDate(urlArr, 4)
-		if stime == "err" {
-			io.WriteString(w, fmt.Sprintf("WT: %s", etime))
-		}
-		fmt.Println(time.Now())
-		rows, err := db.Query(`select * from pfwt where src = ? and time BETWEEN ? and ?`, src, stime, etime)
-		if err != nil {
-			print(err)
-			io.WriteString(w, "WT: query error")
-		}
-		fmt.Println(time.Now())
-		var str_build bytes.Buffer
-		var id int64
-		var itime, wt_data string
-		for rows.Next() {
-			err := rows.Scan(&id, &itime, &src, &wt_data)
-			if err != nil {
-				print(err)
-				io.WriteString(w, "WT: scan error")
-			}
-			//print(fmt.Sprintf("%d: %s %s\n", id, time, wt_data))
-			str_build.WriteString(wt_data)
-			str_build.WriteString("\n")
-		}
-		fmt.Println(time.Now())
-		output := str_build.String()
-		io.WriteString(w, output)
-		fmt.Println(time.Now())
-		return
-	case "pfwta":
-		if len(urlArr) < 9 {
-			io.WriteString(w, "WTA: too few arguments")
-			return
-		}
-		stime, etime := parseDate(urlArr, 3)
-		if stime == "err" {
-			io.WriteString(w, fmt.Sprintf("WTA: %s", etime))
-		}
-		fmt.Println(time.Now())
-		rows, err := db.Query(`select * from pfwt where time BETWEEN ? and ?`, stime, etime)
-		if err != nil {
-			print(err)
-			io.WriteString(w, "WTA: query error")
-		}
-		fmt.Println(time.Now())
-		var str_build bytes.Buffer
-		var id, src int64
-		var itime, wt_data string
-		for rows.Next() {
-			err := rows.Scan(&id, &itime, &src, &wt_data)
-			if err != nil {
-				print(err)
-				io.WriteString(w, "WTA: scan error")
-			}
-			//print(fmt.Sprintf("%d: %s %s\n", id, time, wt_data))
-			str_build.WriteString(wt_data)
-			str_build.WriteString("\n")
-		}
-		fmt.Println(time.Now())
-		output := str_build.String()
-		io.WriteString(w, output)
-		fmt.Println(time.Now())
-		return
-	default:
-		io.WriteString(w, "unknown path:\n"+urlPath)
-		return
-	}
-
-	io.WriteString(w, "test\n"+urlPath)
-}
-
-// assetsFileHandler for static Data
-func assetsFileHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet && r.Method != http.MethodHead {
-		return
-	}
-
-	file := r.URL.Path
-	//	log.Printf("Open File '%s'",file)
-	if file == "/" {
-		file = "/index.html"
-	}
-	f, err := assetsDir.Open(file)
-	if err != nil {
-		log.Printf("can't open file %s: %v\n", file, err)
-		file = "/index.html"
-		f, err = assetsDir.Open(file)
-	}
-	defer f.Close()
-
-	fi, err := f.Stat()
-	if err != nil {
-		log.Printf("can't open file %s: %v\n", file, err)
-		return
-	} else {
-		log.Printf("read[%s]\n", file)
-	}
-	//	log.Print(w.Header())
-	if strings.HasSuffix(file, ".js") {
-		w.Header().Add("Content-Type", "text/javascript")
-	}
-	http.ServeContent(w, r, file, fi.ModTime(), f)
-}
-
-func runSocketIOServer() *gosocketio.Server {
-	server := gosocketio.NewServer(nil)
-	if server == nil {
-		log.Print("Socket IO open error: nil")
-	}
-
-	server.OnConnect("/", func(c gosocketio.Conn) error {
-		c.SetContext("")
-		// wait for a few milli seconds.
-		resp := server.JoinRoom("/", "#", c)
-		log.Printf("Connected ID: %s from %v with URL:%s  %t", c.ID(), c.RemoteAddr(), c.URL().Path, resp)
-		return nil
-	})
-
-	server.OnDisconnect("/", func(c gosocketio.Conn, reason string) {
-		resp := server.LeaveRoom("/", "#", c)
-		log.Printf("Disconnected ID:%s from %v leave:%t reason:%s", c.ID(), c.RemoteAddr(), resp, reason)
-		// need to reconnect?
-	})
-
-	return server
-}
-
-func supplyPCounterCallback(clt *sxutil.SXServiceClient, sp *api.Supply) {
+func supplyPCINFDBCallback(clt *sxutil.SXServiceClient, sp *api.Supply) {
 	mu.Lock()
 	if sp.SupplyName == "PCounterMulti" {
 		log.Printf("Broadcast PCS (%4d bytes)", len(sp.Cdata.Entity))
@@ -402,37 +148,9 @@ func supplyPCounterCallback(clt *sxutil.SXServiceClient, sp *api.Supply) {
 	mu.Unlock()
 }
 
-func supplyPFlowCallback(clt *sxutil.SXServiceClient, sp *api.Supply) {
-	mu.Lock()
-	if sp.SupplyName == "PFlow Supply" {
-		pf := &protoPF.PFlow{}
-		err := proto.Unmarshal(sp.Cdata.Entity, pf)
-		if err == nil {
-			pfj := &PFJ{
-				ID:         pf.Id,
-				FromSID:    pf.Operation[0].Sid,
-				FromTime:   pf.Operation[0].Timestamp.AsTime().UnixNano() / 1000000,
-				FromHeight: pf.Operation[0].Height,
-				ToSID:      pf.Operation[1].Sid,
-				ToTime:     pf.Operation[1].Timestamp.AsTime().UnixNano() / 1000000,
-				ToHeight:   pf.Operation[1].Height,
-			}
-			// log.Printf("Broadcast PF (%4d bytes) %+v", len(sp.Cdata.Entity), pfj)
-			jsonBytes, _ := json.Marshal(pfj)
-			ioserv.BroadcastToRoom("/", "#", "pf", jsonBytes)
-		} else {
-			log.Printf("Unmarshaling err PF: %v", err)
-		}
-	} else {
-		log.Printf("Broadcast Unknown (%4d bytes)", len(sp.Cdata.Entity))
-		ioserv.BroadcastToRoom("/", "#", "unk_pf", sp.Cdata.Entity)
-	}
-	mu.Unlock()
-}
-
 func monitorStatus() {
 	for {
-		sxutil.SetNodeStatus(int32(runtime.NumGoroutine()), "PCWA")
+		sxutil.SetNodeStatus(int32(runtime.NumGoroutine()), "PCINFDB")
 		time.Sleep(time.Second * 3)
 	}
 }
@@ -441,9 +159,9 @@ func main() {
 	flag.Parse()
 	logrus.SetLevel(logrus.InfoLevel)
 
-	channelTypes := []uint32{pbase.PEOPLE_COUNTER_SVC, pbase.PEOPLE_FLOW_SVC}
+	channelTypes := []uint32{pbase.PEOPLE_COUNTER_SVC}
 	var rerr error
-	sxServerAddress, rerr = sxutil.RegisterNode(*nodesrv, "PCWA", channelTypes, nil)
+	sxServerAddress, rerr = sxutil.RegisterNode(*nodesrv, "PCINFDB", channelTypes, nil)
 	if rerr != nil {
 		log.Fatal("Can't register node ", rerr)
 	}
@@ -454,41 +172,16 @@ func main() {
 
 	wg := sync.WaitGroup{} // for syncing other goroutines
 
-	ioserv = runSocketIOServer()
-	fmt.Printf("Running PCWT Server..\n")
-	if ioserv == nil {
-		os.Exit(1)
-	}
-
 	client := sxutil.GrpcConnectServer(sxServerAddress) // if there is server address change, we should do it!
 
-	argJSON := fmt.Sprintf("{Client:PCountWebApp}")
+	argJSON := fmt.Sprintf("{Client:PCountInfoDB}")
 
 	pcClient := sxutil.NewSXServiceClient(client, pbase.PEOPLE_COUNTER_SVC, argJSON)
-	pfClient := sxutil.NewSXServiceClient(client, pbase.PEOPLE_FLOW_SVC, argJSON)
-
-	pcMu, pcLoop = sxutil.SimpleSubscribeSupply(pcClient, supplyPCounterCallback)
-	pfMu, pfLoop = sxutil.SimpleSubscribeSupply(pfClient, supplyPFlowCallback)
+	pcMu, pcLoop = sxutil.SimpleSubscribeSupply(pcClient, supplyPCINFDBCallback)
 
 	wg.Add(1)
 
-	go monitorStatus() // keep status
-
-	assetsDir = http.Dir(*assetDir)
-	log.Println("AssetDir:", assetsDir)
-
-	go ioserv.Serve()
-
-	http.Handle("/pwt/socket.io/", ioserv)
-	http.HandleFunc("/api/", apiHandler)
-	http.HandleFunc("/", assetsFileHandler)
-
-	log.Printf("Starting PCounter Web App Provider %s  on port %d", version, *port)
-	log.Printf("Go to http://localhost:%d", *port)
-	err := http.ListenAndServe(fmt.Sprintf("0.0.0.0:%d", *port), nil)
-	if err != nil {
-		log.Fatal(err)
-	}
+	log.Printf("Starting PCounter Information DB Provider %s  on port %d", version, *port)
 
 	wg.Wait()
 
